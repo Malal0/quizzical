3d17e180ca816cdc090798bf801e073ee2865543
UncomfortableChai <mj1055119@gmail.com>
2022-06-09 02:46:34 -0700

    created array for all answers

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -4,11 +4,22 @@ import TurnIn from "./TurnIn"
 export default function Questionnaire(props) {
     const qArray = props.data
 
+
+
     const questions = qArray.map(ques => {
+        let allAnswers = []
+
+        ques.incorrect_answers.forEach(wrongAnswer => {
+            allAnswers.push(wrongAnswer)
+        });
+        allAnswers.push(ques.correct_answer)
+        console.log(allAnswers)
         return (
             < Question question={ques.question} answers={ques.correct_answer} />
         )
+
     })
+
     return (
         <div>
             {questions}
