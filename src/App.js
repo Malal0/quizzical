5fc0365ddca643ddf8411531c30f2383f0257068
UncomfortableChai <mj1055119@gmail.com>
2022-06-10 18:06:49 -0700

    moved the question components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Title from "./components/Title"
-import Questionnaire from "./components/Questionnaire"
+import Question from "./components/Question"
+import TurnIn from "./components/TurnIn"
 
 export default function App() {
     const [quizStarted, setQuizStarted] = React.useState(false)
@@ -23,10 +24,32 @@ export default function App() {
         console.log("checking answers")
     }
 
+    function answerAction(e) {
+        console.log(e.target.innerText)
+        console.log(e.target.parentNode.parentNode)
+    }
+
+    const questions = questionsData.map(ques => {
+        let allAnswers = []
+
+        ques.incorrect_answers.forEach(wrongAnswer => {
+            allAnswers.push(wrongAnswer)
+        });
+        allAnswers.push(ques.correct_answer)
+        //console.log(allAnswers)
+        return (
+            < Question question={ques.question} answers={allAnswers} correctAnswer={ques.correct_answer} handleClick={answerAction} />
+        )
+
+    })
+
     return (
         <div className="App">
             {quizStarted ?
-                <Questionnaire data={questionsData} /> :
+                <div>
+                    {questions}
+                    <TurnIn />
+                </div> :
                 <Title handleClick={startQuiz} />}
         </div>
     )
--- src/components/Question.js
+++ src/components/Question.js
@@ -5,10 +5,10 @@ export default function Question(props) {
         <div className="questionContainer">
             <h3 className="question">{props.question}</h3>
             <div className="answerContainer">
-                <button className="answer incorrect">{answersArray[0]}</button>
-                <button className="answer chosen">{answersArray[1]}</button>
-                <button className="answer">{answersArray[2]}</button>
-                <button className="answer correct">{answersArray[3]}</button>
+                <button className="answer incorrect" onClick={props.handleClick}>{answersArray[0]}</button>
+                <button className="answer chosen" onClick={props.handleClick}>{answersArray[1]}</button>
+                <button className="answer" onClick={props.handleClick}>{answersArray[2]}</button>
+                <button className="answer correct" onClick={props.handleClick}>{answersArray[3]}</button>
             </div>
         </div>
     )
